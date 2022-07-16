389a3fd89c06709113e30faddfd90b5f4ea860a2
Enache Octavian <enacheioan11@yahoo.com>
2022-07-16 10:47:41 +0300

    add news refresh functionality

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -12,7 +12,35 @@ export class NewsComponent implements OnInit {
   url = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml";
   news: News[] = [];
 
+  onRefreshNews() {
+    this.news = [];
+    let xmlContent = '';
 
+    fetch(this.url).then((response) => {
+      response.text().then((xml) => {
+        xmlContent = xml;
+
+        let parser = new DOMParser();
+        let xmlDOM = parser.parseFromString(xmlContent, 'application/xml');
+        let news = xmlDOM.querySelectorAll('item');
+
+        news.forEach(newsXmlNode => {
+          let test = {
+            title: '',
+            author: '',
+            content: '',
+            date: ''
+          };
+          test.title = newsXmlNode.querySelectorAll('title')[0].innerHTML;
+          test.date = newsXmlNode.querySelectorAll('pubDate')[0].innerHTML;
+          test.author = newsXmlNode.children[5].innerHTML;
+          test.content = newsXmlNode.querySelectorAll('description')[0].innerHTML;
+          this.news.push(test);
+          console.log("test");
+        });
+      });
+    });
+  }
 
 
   ngOnInit(): void {
